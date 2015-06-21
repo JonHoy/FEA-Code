4fc1b11cf641c92614fa2d29b4b02ba8282a28e8
Jonathan Hoy <jonhoy34@gmail.com>
2015-06-20 21:02:01 -0700

    Implementing NURBS Curves and Surfaces, and Piecewise Polynomials

---
+++ FEA/FEA.Assembler/Piecewise.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FEA.Assembler
+{
+    public class Piecewise // this class implements piecewise polynomials over the domain [-inf, inf]
+    {
+        // The value of the piecewise polynomial is zero between [-inf , start] and [end, inf]
+        public Piecewise(double Start, double End, Polynomial P)
+        {
+            if (!(End > Start))
+                throw new Exception("End must be greater than Start");
+            x = new double[]{ Start, End };
+            Polynomials = new Polynomial[1];
+            Polynomials[0] = P;
+        }
+        public Piecewise(double[] Points, Polynomial[] Polys) {
+            if ((Points.Length - Polys.Length) != 1)
+                throw new Exception("Points must be 1 element more than Polys");
+            Polynomials = Polys;
+            x = Points; 
+        }
+        public static Piecewise operator+(Piecewise p1, Piecewise p2) {
+            var x = MakeVector(p1, p2);
+            var Polys = new Polynomial[x.Length - 1];
+            for (int i = 0; i < Polys.Length; i++)
+            {
+                double Val = (x[i] + x[i + 1]) / 2.0;
+                int id1 = p1.Index(Val);
+                int id2 = p2.Index(Val);
+                if (id1 > -1 && id2 > -1)
+                {
+                    Polys[i] = p1.Polynomials[id1] + p2.Polynomials[id2];
+                }
+                else if (id1 > -1) {
+                    Polys[i] = p1.Polynomials[id1];
+                }
+                else if (id2 > -1) {
+                    Polys[i] = p2.Polynomials[id2];
+                }
+                else
+                {
+                    Polys[i] = new Polynomial(new double[]{ 0 });
+                }
+            }
+            return new Piecewise(x, Polys);
+        }
+        public static Piecewise operator-(Piecewise p1, Piecewise p2) {
+            p2 = p2 * -1.0;
+            return p1 + p2;
+        }
+        public static Piecewise operator*(Piecewise p1, Piecewise p2) {
+            var xnew = MakeVector(p1, p2);
+            var Polys = new Polynomial[xnew.Length - 1];
+            for (int i = 0; i < Polys.Length; i++)
+            {
+                double Val = (xnew[i] + xnew[i + 1]) / 2.0;
+                int id1 = p1.Index(Val);
+                int id2 = p2.Index(Val);
+                if (id1 > -1 && id2 > -1)
+                {
+                    Polys[i] = p1.Polynomials[id1] * p2.Polynomials[id2];
+                }
+                else
+                {
+                    Polys[i] = new Polynomial(new double[]{ 0 });
+                }
+            }
+            return new Piecewise(xnew, Polys);
+        }
+        public static Piecewise operator*(Piecewise p1, Polynomial p) {
+            var pnew = new Polynomial[p1.x.Length - 1];
+            var xnew = p1.x;
+            for (int i = 0; i < pnew.Length; i++)
+            {
+                pnew[i] = p1.Polynomials[i] * p;
+            }
+            return new Piecewise(xnew, pnew);
+        }
+        public static Piecewise operator*(Piecewise p1, double a) {
+            var pnew = new Polynomial[p1.x.Length - 1];
+            var xnew = p1.x;
+            for (int i = 0; i < pnew.Length; i++)
+            {
+                pnew[i] = p1.Polynomials[i] * a;
+            }
+            return new Piecewise(xnew, pnew);
+        }
+
+        private static double[] MakeVector(Piecewise p1, Piecewise p2) {
+            var xnew = new HashSet<double>();
+            for (int i1 = 0; i1 < p1.x.Length; i1++)
+            {
+                xnew.Add(p1.x[i1]);
+            }
+            for (int i2 = 0; i2 < p2.x.Length; i2++)
+            {
+                xnew.Add(p2.x[i2]);
+            }
+            var x = xnew.ToArray();
+            Array.Sort(x);
+            return x;
+        }
+        public int Index(double Val) {
+            // if in bounds it returns the index number of the specified value else
+            int Id = -1;
+            for (int i = 1; i < x.Length; i++)
+            {
+                if (Val <= x[i] && Val >= x[i - 1])
+                {
+                    Id = i - 1;
+                    break;
+                }
+            }
+            return Id;
+        }
+        public double Evaluate(double Val) { // evaluates the piecewise polynomial
+            // discontinuities evaluate on the negative side 
+            // ie (Y = a -> [-1 , 1] and Y = b [1 , 2]) thus, Y(1) = a
+            double Ans = 0;
+            int Location = Index(Val);
+            if (Location != -1)
+            {
+                Ans = Polynomials[Location].Evaluate(Val);
+            }
+            return Ans;
+        }
+        public void Simplify() {
+            // this function removes Polynomials on the edges of the domain which equal zero
+        }
+        double[] x; // monotomically unique increasing vector
+        Polynomial[] Polynomials;
+    }
+}
+
---
+++ FEA/FEA.Mesher/Rational_BSpline_Surface.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FEA.Mesher.IGES
+{
+    // Reference IGES SPEC v6.0 1998-01-05 pdf pp 166
+    public class Rational_BSpline_Surface
+    {
+        public Rational_BSpline_Surface(double[] Parameters)
+        {
+            K1 = (int)Parameters[1];
+            K2 = (int)Parameters[2];
+            M1 = (int)Parameters[3];
+            M2 = (int)Parameters[4];
+            PROP1 = (int)Parameters[5];
+            PROP2 = (int)Parameters[6];
+            PROP3 = (int)Parameters[7];
+            PROP4 = (int)Parameters[8];
+            PROP5 = (int)Parameters[9];
+            int N1 = 1 + K1 - M1;
+            int N2 = 1 + K2 - M2;
+            int A = N1 + 2 * M1;
+            int B = N2 + 2 * M2;
+            int C = (1 + K1) * (1 + K2);
+        }
+        int K1; // Upper index of first sum. See Appendix B
+        int K2; // Upper index of second sum. See Appendix B
+        int M1; // Degree of first set of basis functions
+        int M2; // Degree of second set of basis functions
+        int PROP1; // 1 = Closed in first parametric variable direction
+        int PROP2; // 1 = Closed in second parametric variable direction
+        int PROP3; // 0 = Rational, 1 = Polynomial
+        int PROP4; // 1 = Periodic in first parametric variable direction
+        int PROP5; // 1 = Periodic in second parametric variable direction
+        double[] S; // first knot sequence
+        double[] T; // second knot sequence
+        double[,] W; // weights
+        double[,] PTS; // Control Points
+        double U0; // Starting value for first parametric direction
+        double U1; // Ending value for first parametric direction
+        double V0; // Starting value for second parametric direction
+        double V1; // Ending value for second parametric direction
+    }
+}
+
