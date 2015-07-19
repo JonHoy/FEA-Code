f8d12f9f957aed562b2c2b85b069725762e3089c
Jonathan Hoy <jonhoy34@gmail.com>
2015-07-19 15:02:34 -0700

    Adding in Plane Ray Intersection

--- FEA/FEA.Mesher/Plane.cuh
+++ FEA/FEA.Mesher/Plane.cuh
@@ -5,6 +5,19 @@
 template <typename T>
 struct Plane {
 
+    Plane() {}
+    Plane(Triangle<T> Tri) { // gets the
+    	Tri.NormalVector();
+    	N = Tri.N;
+    	// now solve for d using pt A
+    	d = -1.0 * Tri.A.Dot(N)
+    }
+    // http://www.cs.princeton.edu/courses/archive/fall2000/cs426/lectures/raycast/sld017.htm
+    T Intersection(Vector<T> O, Vector<T> D) {
+    	T tval = (-1.0 * O.Dot(N) + d) / D.Dot(N);
+    	return tval;
+    }
+    
     Vector<T> N; //(ax + by + cy = d) 
 	T d; // Where a = N.x, b = N.y, c = N.z
 	
