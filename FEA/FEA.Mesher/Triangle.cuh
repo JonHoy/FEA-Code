44a566b78af5a3720c460fe0e100a51478431fd1
Jonathan Hoy <jonhoy34@gmail.com>
2015-07-21 07:16:30 -0700

    Adding in Utilities Project

--- FEA/FEA.Mesher/Triangle.cuh
+++ FEA/FEA.Mesher/Triangle.cuh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Vector.cuh"
+#include "../FEA.Assembler/Inverse.cuh"
 
 #define EPSILON 0.000001
 	
@@ -19,56 +20,24 @@ struct Triangle
 
     __device__ T Intersection(Vector<T> O, Vector<T> D)
     {
-	    // https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
-	    // there are three possible outcomes
-	    // No intersection (return t = -1)
-	    // Point intersection (intersection in the triangle return t > 0)
-	    // line intersection (Line is in the plane return t = -2)
-	    
-	    // construct 3 rays 
-	    
-	    Vector<T> e1, e2, P, Q, S;
-	    T det, inv_det, u, v, t;
-	    //Find vectors for two edges sharing V1
-	    e1 = B - A;
-	    e2 = C - A;
-	    //Begin calculating determinant - also used to calculate u parameter
-	    P = D.Cross(e2);
-	    //if determinant is near zero, ray lies in plane of triangle
-	    det = e1.Dot(P);
-	    
-	    if(det > -EPSILON && det < EPSILON) {
-	    	return -1.0;
-	    }
-	    
-	    inv_det = 1.0 / det;
-	    //calculate distance from V1 to ray origin
-	    S = O - A;
-	    //Calculate u parameter and test bound
-	    u = S.Dot(P) * inv_det;
-	    //The intersection lies outside of the triangle
-	    if(u < 0.0 || u > 1.0) {
-	    	return -1.0;
-	    }
-	    //Prepare to test v parameter
-	    Q = S.Cross(e1);
-	    //Calculate V parameter and test bound
-	    v = D.Dot(Q) * inv_det;
-	    
-	      //The intersection lies outside of the triangle
-	    if(v < 0.f || u + v  > 1.f) {
-	    	return -1.0;
-	    }
-	    
-	    t = e2.Dot(Q) * inv_det;
-	    
-	    //ray intersection
-	    if (t > EPSILON) {
-	    	return t;
-	    }
-	    // No hit, no win
-	    return -1.0;
-	    
+		NormalVector();
+		T d = -1.0 * A.Dot(N);
+		T t = -1.0 * (O.Dot(N) + d) / D.Dot(N);
+		
+		Vector<T> Zprime = N;
+		Zprime.Normalize();
+		Vector<T> Xprime = B - A;
+		Xprime.Normalize();
+		Yprime = Zprime.Cross(Xprime);
+		
+		Vector<T> P = O + D*t;
+		T px = P.Dot(Xprime);
+		T py = P.Dot(Yprime);
+		
+		Vector<T> CA = C - A;
+		Vector<T> BA = B - A;
+		
+	    return t;
     }
     
     __device__ void NormalVector() { // computes the normal vector in place
---
+++ FEA/Utilities/CS2CPP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utilities
+{
+    // writes a c# class to cpp/ cuda code
+    public class CS2CPP
+    {
+        public CS2CPP(List<List<string>> CSCode)
+        {
+            
+        }
+        // TODO Handle transform a c# generic to a template
+        // Make template instatiation dynamic ie (have os compile code at runtime)
+        // The purpose of this class is to better suport META programming
+        // Memory Layout must be sequential in the C# class
+
+        // Header files must be automatically generated and included (use header generation tool
+
+        Dictionary<string, int> ClassNames; // dictionary that holds the index location of the class in the CodeList
+        // string is the class name
+
+
+    }
+    public class ClassProperties {
+        
+    }
+}
+
